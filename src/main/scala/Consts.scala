6dd46ea11dec09678324e7c4fab7fbd18d0e0a3f
Aleksandra Karbarczyk <akarbarczyk@gmail.com>
2015-06-02 18:57:32 +0200

    Big map

--- src/main/scala/Consts.scala
+++ src/main/scala/Consts.scala
@@ -1,14 +1,14 @@
 import scala.concurrent.duration._
 
 object Consts {
-  val tileSize = 20
-  val mapColumnsCount = 10 * 2
-  val mapRowsCount = 10 * 2
-  val carsCount = 20
+  val tileSize = 16
+  val mapColumnsCount = 37 * 2
+  val mapRowsCount = 33 * 2
+  val carsCount = 200
   val windowWidth = mapColumnsCount * tileSize
   val windowHeight = mapRowsCount * tileSize
   val lightSize = 15
   val carVelocity = 1 seconds
   val waitingSchedulerSpan = 3 seconds
-  val mapFilename = "complex_map.txt"
+  val mapFilename = "big_map.txt"
 }
