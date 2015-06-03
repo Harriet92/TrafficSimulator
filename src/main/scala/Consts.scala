9a40a717950341d00cc5a9be0eafcc4534f36fd4
Aleksandra Karbarczyk <akarbarczyk@gmail.com>
2015-06-04 00:09:04 +0200

    Final map

--- src/main/scala/Consts.scala
+++ src/main/scala/Consts.scala
@@ -1,13 +1,13 @@
 import scala.concurrent.duration._
 
 object Consts {
-  val tileSize = 20
-  val mapColumnsCount = 10 * 2
-  val mapRowsCount = 10 * 2
-  val carsCount = 20
+  val tileSize = 18
+  val mapColumnsCount = 20 * 2
+  val mapRowsCount = 20 * 2
+  val carsCount = 50
   val windowWidth = mapColumnsCount * tileSize
   val windowHeight = mapRowsCount * tileSize
   val lightSize = 15
   val carVelocity = 1 seconds
-  val mapFilename = "complex_map.txt"
+  val mapFilename = "final_map.txt"
 }
